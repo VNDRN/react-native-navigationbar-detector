8e0369d180e4d99a76e3ea2676dc347942693497
Jef Vandooren <jef.vandooren@inthepocket.com>
2023-08-24 08:47:44 +0200

    Revert "🐛 fix ios build"
    This reverts commit 89553491ffd08c3a0704b0842d8dbb19dbf14848.

---
+++ ios/ReactNativeNavigationbarDetectorModule.swift
@@ -0,0 +1,20 @@
+import ExpoModulesCore
+
+public class ReactNativeNavigationbarDetectorModule: Module {
+  // Each module class must implement the definition function. The definition consists of components
+  // that describes the module's functionality and behavior.
+  // See https://docs.expo.dev/modules/module-api for more details about available components.
+  public func definition() -> ModuleDefinition {
+    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
+    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
+    // The module will be accessible from `requireNativeModule('ReactNativeNavigationbarDetector')` in JavaScript.
+    Name("ReactNativeNavigationbarDetector")
+
+    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+  //  Function("getNavigationBarTypeInteger") {
+  //     return "ios not yet supported";
+  //   }
+
+    
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
+import { Platform } from "react-native";
+import ReactNativeNavigationbarDetectorModule from "./ReactNativeNavigationbarDetectorModule";
+import { NavigationBarType } from "./ReactNativeNavigationbarDetector.types";
+export * from "./ReactNativeNavigationbarDetector.types";
+
+const getAndroidNavigationBarTypeForInteger = (integer: number): NavigationBarType => {
+  switch (integer) {
+    case 0:
+      return "threeButton";
+    case 1:
+      return "twoButton";
+    case 2:
+      return "gesture";
+    default:
+      return "threeButton";
+  }
+};
+
 export function getNavigationBarType() {
+  if (Platform.OS === "android") {
+    const integer = ReactNativeNavigationbarDetectorModule.getNavigationBarTypeInteger();
+    return getAndroidNavigationBarTypeForInteger(integer);
+  }
   //TODO: add support for iOS
-  return "ios or web not yet supported!";
+  return "ios not yet supported";
 }
